4752a22d9a2a2803b5011ea0d2d3cc7f2e38c61b
Hiago Zavarize <hiagozavarizex2@gmail.com>
2024-09-07 00:19:59 -0300

    feat: creating home component

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+import ListOfPopularMovies from "../components/listOfPopularMovies";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <ListOfPopularMovies />
+    </div>
+  );
+}
