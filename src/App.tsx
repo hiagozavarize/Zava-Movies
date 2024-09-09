c53fa34930f64cef7e2853a6f48fe1df85992160
Hiago Zavarize <hiagozavarizex2@gmail.com>
2024-09-09 16:08:30 -0300

    feat: adding new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Home from "./pages/home";
+import SearchResults from "./pages/searchResults";
+import { MovieDetails } from "./components/movieDetails";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="w-full h-full bg-BgContainerColor ">
-      <Home />;
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filmes" element={<SearchResults />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
